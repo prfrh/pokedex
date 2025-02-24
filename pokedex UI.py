3111dc4cc106fb47b560c4603b4a6317485ec65e
prfrh <rahaid.iqbal.351@accesscreative.ac.uk>
2025-02-24 16:01:20 +0000

    Add files via upload

--- pokedex UI.py
+++ pokedex UI.py
@@ -1,10 +1,10 @@
+from tkinter import *
 import tkinter as tk
 
 
 
-
 usernamestored = 'rh'
-userpasswordstored = 'Pokedex123!'
+userpasswordstored = 'pokedex'
 
 def login():
     while True:
@@ -12,35 +12,55 @@ def login():
         password = input("Enter a password. ")
         if user == usernamestored and password == userpasswordstored:
             print ("Welcome RH...")
+            break
         else:
             print ("Incorrect login, try again.")
-
-    
-
-
+            
+        
 
 
 
 
 window = tk.Tk()
+window.title('Pokedex login')
 
-label = tk.Label(
-    text="Hello, Tkinter",
-    foreground="black",  # Set the text color to white
-    background="grey",  # Set the background color to black
-    width=75, 
-    height=50,
-)
-label.pack()
+menulabel = Tk()
+Label(window, text='user name').grid(row=0)
+Label(window, text='password').grid(row=1)
+menulabel.geometry("10x50")
+background="grey"
+e1 = Entry(window)
+e2 = Entry(window)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
 
-button = tk.Button(
-    text="Click me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="orange",
-)
-button.pack()
+
+
+top = Toplevel()
+top.geometry("200x100")
+top.title("Pokedex")
+l2 = Label(top, text = "Welcome rh")
+l2.pack()
+
+# label = tk.Label(
+#     text="Hello, Tkinter",
+#     foreground="black",  # Set the text color to white
+#     background="grey",  # Set the background color to black
+#     width=75, 
+#     height=50,
+# )
+# label.pack()
+
+# button = tk.Button(
+#     text="Click me!",
+#     width=25,
+#     height=5,
+#     command=window.destroy,
+#     bg="blue",
+#     fg="orange",
+# )
+# button.pack()
 
 
 window.mainloop()
+
